e0840eff00970de5d83e7bd1c8109d58843aa6b2
fardin36 <thunder.fardin@gmail.com>
2023-09-14 21:40:16 +0600

    Added Static Card Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import './App.css'
+import Card from './Card'
 
 function App() {
 
   return (
     <>
-      <h1>Hello World!</h1>
+      <main className='px-14 pb-20'>
+        <h1 className='text-center text-4xl font-bold pt-12 pb-8'>Course Registration</h1>
+        <div className='flex justify-between gap-6'>
+          <div className='w-3/4 flex justify-between gap-6 flex-wrap'>
+            <Card></Card>
+          </div>
+          <div className='w-1/4'>
+
+          </div>
+        </div>
+      </main>
     </>
   )
 }
